09d0f40a6b176c2d8b63868bf71bceea25358cd2
snowsee <ssnowsee@gmail.com>
2019-11-20 21:20:07 +0000

    Added brawl box with gold and powerpoints

--- brawlcord/utils.py
+++ brawlcord/utils.py
@@ -1,32 +1,30 @@
-from .brawlers import Brawler
+import random
 
 import discord
 
-class GameSession:
-    """A class to represent and hold current game sessions per server."""
+from brawlcord.brawlers import emojis, brawler_emojis
 
-    timer: int
-    guild: discord.Guild
-    message_id: int
-    reacted: bool = False
-    teamred: Set[discord.Member] = set()
-    teamblue: Set[discord.Member] = set()
+# class GameSession:
+#     """A class to represent and hold current game sessions per server."""
 
-    def __init__(self, **kwargs):
-        self.guild: dict = kwargs.pop("guild")
-        self.timer: int = kwargs.pop("timer")
-        self.reacted = False
-        self.message_id = 0
-        teamred: Set[discord.Member] = set()
-        teamblue: Set[discord.Member] = set()
+#     timer: int
+#     guild: discord.Guild
+#     message_id: int
+#     reacted: bool = False
+#     teamred: Set[discord.Member] = set()
+#     teamblue: Set[discord.Member] = set()
+
+#     def __init__(self, **kwargs):
+#         self.guild: dict = kwargs.pop("guild")
+#         self.timer: int = kwargs.pop("timer")
+#         self.reacted = False
+#         self.message_id = 0
+#         teamred: Set[discord.Member] = set()
+#         teamblue: Set[discord.Member] = set()
 
 
 class Box:
     """A class to represent Boxes."""
-
-    gold: int
-    powerpoints: int
-    powerpoints_stacks = int
     
     # odds
     resources_1: int
@@ -46,6 +44,10 @@ class Box:
     
     def __init__(self, all_brawlers, brawler_data):        
         for brawler in brawler_data:
+            # if all_brawlers[brawler]["unlockTrp"] >= 0:
+            #     # it is a trophy road brawler 
+            #     continue
+
             # self.owned.append(brawler)
             total_powerpoints = brawler_data[brawler]['total_powerpoints']
             if total_powerpoints < self.max_pp:
@@ -65,9 +67,99 @@ class Box:
                 else:
                     pass
 
-    def brawlbox(self):
-        """"""
+    def weighted_random(self, lower, upper, avg):
+        avg_low = (avg + lower) / 2
+        avg_high = (upper + avg) / 2
+        
+        p_high = (avg - avg_low) / (avg_high - avg_low)
 
+        # return p_high
 
+        chance = random.random() 
+
+        if chance < p_high:
+            return random.randint(avg, upper)
+        else:
+            return random.randint(lower, avg)
+    
+    def split_in_integers(self, number, num_of_pieces):
+        """Split a number into number of integers."""
+        def accel_asc(n):
+            a = [0 for i in range(n + 1)]
+            k = 1
+            y = n - 1
+            while k != 0:
+                x = a[k - 1] + 1
+                k -= 1
+                while 2 * x <= y:
+                    a[k] = x
+                    y -= x
+                    k += 1
+                l = k + 1
+                while x <= y:
+                    a[k] = x
+                    a[l] = y
+                    yield a[:k + 2]
+                    x += 1
+                    y -= 1
+                a[k] = x + y
+                y = x + y - 1
+                yield a[:k + 1]
+        
+        pieces = list(accel_asc(number))
+        random.shuffle(pieces)
+        
+        for piece in pieces:
+            if len(piece) == num_of_pieces:
+                return piece
+
+    async def brawlbox(self, conf, user):
+        """Function to handle brawl box openings."""
+    
+        gold = self.weighted_random(12, 70, 19) 
+
+        stacks = 2
+        
+        if len(self.can_get_pp) == 0:
+            gold *= 3
+            stacks = 0
+
+        elif len(self.can_get_pp) == 1:
+            gold *= 2
+            stacks = 1
+
+        powerpoints = int(self.weighted_random(9, 25, 16))
+
+        pieces = self.split_in_integers(powerpoints, stacks)
+        
+        pp_str = ""
+        
+        if pieces:
+            for piece in pieces:
+                items = list(self.can_get_pp.items())
+                random.shuffle(items)
+                for brawler, threshold in items:
+                    if piece <= threshold:
+                        async with conf.brawlers() as brawlers:
+                            brawlers[brawler]['powerpoints'] += piece
+                            brawlers[brawler]['total_powerpoints'] += piece
+                            pp_str += f"\n{brawler_emojis[brawler]} **{brawler}:** {emojis['powerpoint']} {piece}"
+                    else:
+                        continue
+                    break
+        
+        if not pp_str:
+            pp_str = "No powerpoints"
+        
+        old_gold = await conf.gold()
+        await conf.gold.set(old_gold + gold)
+
+        embed = discord.Embed(color=0xFFA232, title="Brawl Box")
+        embed.set_author(name=user.name, icon_url=user.avatar_url)
+        embed.add_field(name="Gold", value=f"{emojis['gold']} {gold}", inline=False)
+        embed.add_field(name="Power Points", value=pp_str.strip(), inline=False)
+
+        return embed
+        
 class GameModes:
     """A class to represent game modes."""
