4423daa7197ab3878b9a7f89cb87dce57a5728ce
snowsee <ssnowsee@gmail.com>
2019-11-26 15:35:26 +0000

    Removed default invite command

--- brawlcord/__init__.py
+++ brawlcord/__init__.py
@@ -1,7 +1,10 @@
-from .brawlcord import Brawlcord
+from .brawlcord import old_invite, Brawlcord
 
 
 async def setup(bot):
+    old_invite = bot.get_command("invite")
+    if old_invite:
+        bot.remove_command(old_invite.name)
     cog = Brawlcord(bot)
     await cog.initialize()
     bot.add_cog(cog)
