57543b0ecf8f4160f342a6937d39f9d09cbe7699
snowsee <ssnowsee@gmail.com>
2020-04-06 01:53:33 +0530

    Moved setup to brawlcord.py

--- brawlcord/__init__.py
+++ brawlcord/__init__.py
@@ -1,10 +1,1 @@
-from .brawlcord import Brawlcord
-
-
-async def setup(bot):
-    old_invite = bot.get_command("invite")
-    if old_invite:
-        bot.remove_command(old_invite.name)
-    cog = Brawlcord(bot)
-    await cog.initialize()
-    bot.add_cog(cog)
+from .brawlcord import setup
