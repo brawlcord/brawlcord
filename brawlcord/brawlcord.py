1624a909fc83455b0a7059e549547fa4aa9ed288
snowsee <ssnowsee@gmail.com>
2020-05-15 17:52:37 +0530

    Rearrange tasks and fix small errors

--- brawlcord/brawlcord.py
+++ brawlcord/brawlcord.py
@@ -178,13 +178,9 @@ class Brawlcord(commands.Cog):
                 logging.exception("Error in task", exc_info=exc)
                 print("Error in task:", exc)
 
-        self.bank_update_task = self.bot.loop.create_task(
-            self.update_token_bank()
-        )
+        self.bank_update_task = self.bot.loop.create_task(self.update_token_bank())
         self.status_task = self.bot.loop.create_task(self.update_status())
-        self.shop_and_st_task = self.bot.loop.create_task(
-            self.update_shop_and_st()
-        )
+        self.shop_and_st_task = self.bot.loop.create_task(self.update_shop_and_st())
         self.bank_update_task.add_done_callback(error_callback)
         self.shop_and_st_task.add_done_callback(error_callback)
         self.status_task.add_done_callback(error_callback)
@@ -2038,6 +2034,104 @@ class Brawlcord(commands.Cog):
 
         await menu(ctx, embeds, DEFAULT_CONTROLS)
 
+    # Start Tasks
+
+    async def update_token_bank(self):
+        """Task to update token banks."""
+
+        while True:
+            for user in await self.config.all_users():
+                user = self.bot.get_user(user)
+                if not user:
+                    continue
+                tokens_in_bank = await self.get_player_stat(
+                    user, 'tokens_in_bank')
+                if tokens_in_bank == 200:
+                    continue
+                tokens_in_bank += 20
+                if tokens_in_bank > 200:
+                    tokens_in_bank = 200
+
+                bank_update_timestamp = await self.get_player_stat(user, 'bank_update_ts')
+
+                if not bank_update_timestamp:
+                    continue
+
+                bank_update_ts = datetime.utcfromtimestamp(ceil(bank_update_timestamp))
+                time_now = datetime.utcnow()
+                delta = time_now - bank_update_ts
+                delta_min = delta.total_seconds() / 60
+
+                if delta_min >= 80:
+                    await self.update_player_stat(
+                        user, 'tokens_in_bank', tokens_in_bank)
+                    epoch = datetime(1970, 1, 1)
+
+                    # get timestamp in UTC
+                    timestamp = (time_now - epoch).total_seconds()
+                    await self.update_player_stat(
+                        user, 'bank_update_ts', timestamp)
+
+            asyncio.sleep(60)
+
+    async def update_status(self):
+        """Task to update bot's status with total guilds.
+
+        Runs every 2 minutes.
+        """
+
+        while True:
+            try:
+                await self.bot.change_presence(
+                    activity=discord.Game(
+                        name=f'Brawl Stars in {len(self.bot.guilds)} servers'
+                    )
+                )
+            except Exception:
+                pass
+
+            await asyncio.sleep(120)
+
+    async def update_shop_and_st(self):
+        """Task to update daily shop and star tokens."""
+
+        while True:
+            s_reset = await self.config.shop_reset_ts()
+            create_shop = False
+            if not s_reset:
+                # first reset
+                create_shop = True
+            shop_diff = datetime.utcnow() - datetime.utcfromtimestamp(s_reset)
+
+            st_reset = await self.config.st_reset_ts()
+            reset = False
+            if not st_reset:
+                # first reset
+                reset = True
+                continue
+            st_diff = datetime.utcnow() - datetime.utcfromtimestamp(st_reset)
+
+            for user in await self.config.all_users():
+                user = self.bot.get_user(user)
+                if not user:
+                    continue
+                if create_shop:
+                    await self.create_shop(user)
+                    continue
+                if shop_diff.days > 0:
+                    await self.create_shop(user)
+
+                st_reset = await self.config.st_reset_ts()
+                if reset:
+                    await self.reset_st(user)
+                    continue
+                if st_diff.days > 0:
+                    await self.reset_st(user)
+
+            await asyncio.sleep(300)
+
+    # End Tasks
+
     async def get_player_stat(
         self, user: discord.User, stat: str,
         is_iter=False, substat: str = None
@@ -2295,46 +2389,6 @@ class Brawlcord(commands.Cog):
         if trophies > pb:
             await self.update_player_stat(
                 user, 'brawlers', trophies, substat=brawler, sub_index='pb')
-
-    async def update_token_bank(self):
-        """Task to update token banks."""
-
-        while True:
-            for user in await self.config.all_users():
-                user = self.bot.get_user(user)
-                if not user:
-                    continue
-                tokens_in_bank = await self.get_player_stat(
-                    user, 'tokens_in_bank')
-                if tokens_in_bank == 200:
-                    continue
-                tokens_in_bank += 20
-                if tokens_in_bank > 200:
-                    tokens_in_bank = 200
-
-                bank_update_timestamp = await self.get_player_stat(
-                    user, 'bank_update_ts')
-
-                if not bank_update_timestamp:
-                    continue
-
-                bank_update_ts = datetime.utcfromtimestamp(
-                    ceil(bank_update_timestamp))
-                time_now = datetime.utcnow()
-                delta = time_now - bank_update_ts
-                delta_min = delta.total_seconds() / 60
-
-                if delta_min >= 80:
-                    await self.update_player_stat(
-                        user, 'tokens_in_bank', tokens_in_bank)
-                    epoch = datetime(1970, 1, 1)
-
-                    # get timestamp in UTC
-                    timestamp = (time_now - epoch).total_seconds()
-                    await self.update_player_stat(
-                        user, 'bank_update_ts', timestamp)
-
-            await asyncio.sleep(60)
 
     def get_rank(self, pb):
         """Return rank of the Brawler based on its personal best."""
@@ -2869,22 +2923,6 @@ class Brawlcord(commands.Cog):
             ctx, getattr(error, "original", error), unhandled_by_cog=True
         )
 
-    async def update_status(self):
-        """Task to update bot's status with total guilds.
-
-        Runs every 2 minutes.
-        """
-
-        await self.bot.wait_until_ready()
-        while True:
-            await self.bot.change_presence(
-                activity=discord.Game(
-                    name=f'Brawl Stars in {len(self.bot.guilds)} servers'
-                )
-            )
-
-            await asyncio.sleep(120)
-
     async def create_shop(self, user: discord.User, update=True) -> Shop:
 
         brawler_data = await self.get_player_stat(
@@ -2905,46 +2943,6 @@ class Brawlcord(commands.Cog):
             await self.config.shop_reset_ts.set(timestamp)
 
         return shop
-
-    async def update_shop_and_st(self):
-        """Task to update daily shop and star tokens."""
-
-        await self.bot.wait_until_ready()
-
-        while not self.bot.is_closed():
-            s_reset = await self.config.shop_reset_ts()
-            create_shop = False
-            if not s_reset:
-                # first reset
-                create_shop = True
-            shop_diff = datetime.utcnow() - datetime.utcfromtimestamp(s_reset)
-
-            st_reset = await self.config.st_reset_ts()
-            reset = False
-            if not st_reset:
-                # first reset
-                reset = True
-                continue
-            st_diff = datetime.utcnow() - datetime.utcfromtimestamp(st_reset)
-
-            for user in await self.config.all_users():
-                user = self.bot.get_user(user)
-                if not user:
-                    continue
-                if create_shop:
-                    await self.create_shop(user)
-                    continue
-                if shop_diff.days > 0:
-                    await self.create_shop(user)
-
-                st_reset = await self.config.st_reset_ts()
-                if reset:
-                    await self.reset_st(user)
-                    continue
-                if st_diff.days > 0:
-                    await self.reset_st(user)
-
-            await asyncio.sleep(300)
 
     async def _view_shop(self, ctx: Context):
         """Sends shop embeds."""
