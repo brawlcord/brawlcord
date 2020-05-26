7c41d731a868f9a05ec72e79cd909511701c4dc7
snowsee <ssnowsee@gmail.com>
2020-05-26 14:09:30 +0530

    Version bump: Remove Star Shelly from tutorial

--- brawlcord/brawlcord.py
+++ brawlcord/brawlcord.py
@@ -33,7 +33,7 @@ from .utils import Box, default_stats, maintenance
 
 log = logging.getLogger("red.brawlcord")
 
-__version__ = "2.2.1"
+__version__ = "2.2.2"
 __author__ = "Snowsee"
 
 default = {
